cf7790e0f49ab739fad3dca5bfbe22d73aded3b2
timwright <tim.wright@me.com>
2014-06-23 22:32:11 +0800

    Added a change which attempts to tranform non-unique group keys when a group name is transformed to 16 char group identifier.

--- src/main/java/teamcity/crowd/plugin/loginmodule/LoggedInUserService.java
+++ src/main/java/teamcity/crowd/plugin/loginmodule/LoggedInUserService.java
@@ -25,7 +25,7 @@ import java.util.List;
 import static java.lang.String.format;
 
 public class LoggedInUserService {
-    private final GroupNameToGroupKey groupNameToGroupKey = new GroupNameToGroupKey();
+    private final GroupNameToGroupKey groupNameToGroupKey;
     private final UserGroupManager userGroupManager;
     private final PluginCrowdClient pluginCrowdClient;
     private final UserModel userModel;
@@ -36,7 +36,13 @@ public class LoggedInUserService {
     public static final String ALL_USERS_GROUP = "All Users";
     public static final String CREATED_BY_PLUGIN_MESSAGE = "Created by Crowd Plugin";
 
-    public LoggedInUserService(UserGroupManager userGroupManager, PluginCrowdClient pluginCrowdClient, UserModel userModel, LoggerFactory loggerFactory, CrowdPluginConfiguration crowdPluginConfiguration) {
+    public LoggedInUserService(
+            UserGroupManager userGroupManager,
+            PluginCrowdClient pluginCrowdClient,
+            UserModel userModel,
+            LoggerFactory loggerFactory,
+            CrowdPluginConfiguration crowdPluginConfiguration) {
+        groupNameToGroupKey = new GroupNameToGroupKey(userGroupManager);
         this.userGroupManager = userGroupManager;
         this.pluginCrowdClient = pluginCrowdClient;
         this.userModel = userModel;
@@ -61,7 +67,8 @@ public class LoggedInUserService {
 
     private SUser updateUserDetails(SUser teamCityUser, User user) {
         final jetbrains.buildServer.users.User associatedUser = teamCityUser.getAssociatedUser();
-        if (!associatedUser.getName().equals(user.getDisplayName()) || !associatedUser.getEmail().equals(user.getEmailAddress())) {
+        if ( ! associatedUser.getName().equals(user.getDisplayName()) ||
+             ! associatedUser.getEmail().equals(user.getEmailAddress()) ) {
             teamCityUser.updateUserAccount(user.getName(), user.getDisplayName(), user.getEmailAddress());
         }
         return teamCityUser;
@@ -71,13 +78,20 @@ public class LoggedInUserService {
         Logger logger = loggerFactory.getServerLogger();
         final List<String> userGroupsInCrowd = userGroupsInCrowd(teamCityUser);
         final List<String> allTeamCityGroupsUserIsAMemberOfAlready = allTeamCityUserGroups(teamCityUser);
-        final List<String> listOfGropusUserShouldBeRemovedFrom = groupsToBeRemovedFrom(allTeamCityGroupsUserIsAMemberOfAlready, userGroupsInCrowd);
+        final List<String> listOfGropusUserShouldBeRemovedFrom =
+                groupsToBeRemovedFrom(allTeamCityGroupsUserIsAMemberOfAlready, userGroupsInCrowd);
 
         for (String userGroup : userGroupsInCrowd) {
             if (!allTeamCityGroupsUserIsAMemberOfAlready.contains(userGroup)) {
                 SUserGroup teamCityGroup = userGroupManager.findUserGroupByName(userGroup);
                 if (teamCityGroup == null && shouldCreateGroups) {
-                    teamCityGroup = userGroupManager.createUserGroup(groupNameToGroupKey.transform(userGroup), userGroup, CREATED_BY_PLUGIN_MESSAGE);
+                    Optional<String> groupKey = groupNameToGroupKey.transform(userGroup);
+                    if ( groupKey.isPresent() ) {
+                        teamCityGroup = userGroupManager.createUserGroup(
+                                groupKey.get(),
+                                userGroup,
+                                CREATED_BY_PLUGIN_MESSAGE);
+                    }
                 }
                 if (teamCityGroup != null) {
                     teamCityGroup.addUser(teamCityUser);
@@ -104,7 +118,9 @@ public class LoggedInUserService {
     }
 
     private List<String> userGroupsInCrowd(SUser teamCityUser) {
-        return Lists.newArrayList(Collections2.transform(pluginCrowdClient.getUserGroups(teamCityUser.getUsername()), new Function<Group, String>() {
+        return Lists.newArrayList(Collections2.transform(
+                pluginCrowdClient.getUserGroups(teamCityUser.getUsername()),
+                new Function<Group, String>() {
             @Override
             public String apply(@Nullable Group input) {
                 return input.getName();
@@ -113,7 +129,9 @@ public class LoggedInUserService {
     }
 
     private List<String> allTeamCityUserGroups(SUser teamCityUser) {
-        return Lists.newArrayList(Collections2.transform(teamCityUser.getUserGroups(), new Function<UserGroup, String>() {
+        return Lists.newArrayList(Collections2.transform(
+                teamCityUser.getUserGroups(),
+                new Function<UserGroup, String>() {
             @Override
             public String apply(@Nullable UserGroup input) {
                 return input.getName();
